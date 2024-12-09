20ea188b87b64fbf3d5377050c58f05a0801777b
jncrls <jncrls4217@proton.me>
2024-12-09 00:52:14 -0500

    Week 6 complete

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -36,7 +36,61 @@ const tripsFindByCode = async (req, res) => {
     }
 }
 
+const tripsAddTrip = async (req, res) => {
+    try {
+      const trip = await Model.create({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      });
+      res.send(trip);
+    } catch (err) {
+      res.status(500).send({ error: err.message });
+    }
+  };
+  
+// FIXME: PUT method does not work
+const tripsUpdateTrip = async (req, res) => {
+
+    console.log(req.params);
+    console.log(req.body);
+    const q = await Model
+        .findOneAndUpdate(
+            { 'code': req.params.code },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+
+    if (!q) {
+        return res
+            .status(400)
+            .json(err);
+    }
+    else {
+        return res
+            .status(201)
+            .json(q);
+    }
+}
+
+
 module.exports = {
     tripList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,12 +3,16 @@ const router = express.Router();
 
 const tripsController = require('../controllers/trips');
 
+// Define route for our trips endpoint
 router
     .route('/trips')
-    .get(tripsController.tripList);
+    .get(tripsController.tripList) // GET method
+    .post(tripsController.tripsAddTrip); // POST method
 
+// GET and PUT method routes that require parameter (code)
 router
     .route('/trips/:code')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode) // GET method
+    .put(tripsController.tripsUpdateTrip); // PUT method
 
 module.exports = router;
